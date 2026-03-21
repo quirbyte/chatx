87731486c9a8ce876c5ee7cd37c5b35b57e590f3
Soumyadip Mondal <soumyadipar12@gmail.com>
2026-03-21 23:36:18 +0530

    feat: added signup api route

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -1,9 +1,64 @@
-import { NextRequest,NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import z from "zod";
+import { prisma } from "@/lib/prisma";
 
-export async function POST(req:NextRequest) {
-    const body = req.json();
-    
+const requiredBody = z.object({
+  username: z.string().min(6),
+  email: z.string().email().min(10),
+  password: z.string().min(6),
+});
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const { success, error } = requiredBody.safeParse(body);
+  if (!success) {
+    return NextResponse.json(
+      {
+        msg: "Failed to signup",
+        error,
+      },
+      {
+        status: 400,
+      },
+    );
+  }
+  const { username, email, password } = body;
+  try {
+    const existing_user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (existing_user) {
+      return NextResponse.json(
+        {
+          msg: "User already exists!",
+        },
+        {
+          status: 409,
+        },
+      );
+    }
+    const hashed_password = await bcrypt.hash(password, 10);
+    await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashed_password,
+      },
+    });
     return NextResponse.json({
-        msg: "success"
-    })
+      msg: "Successfully signed up",
+    });
+  } catch (err) {
+    return NextResponse.json(
+      {
+        msg: "Internal server error",
+      },
+      {
+        status: 500,
+      },
+    );
+  }
 }
