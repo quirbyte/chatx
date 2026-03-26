f0a3c95b8b7c34c9a1f7de2add97a44884f844d4
Soumyadip Mondal <soumyadipar12@gmail.com>
2026-03-26 22:52:50 +0530

    added auth middleware

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,7 @@
+import ChatBubble from "@/components/chatbubble";
 import Groupbar from "@/components/groupbar";
 import { CopyIcon } from "lucide-react";
+import { SendIcon } from "lucide-react";
 
 export default function Dashboard(){
     return <div className="absolute h-screen w-screen flex items-center justify-center">
@@ -12,7 +14,7 @@ export default function Dashboard(){
                 <Groupbar title="HWFFWFFF" />
                 <div className="absolute bottom-2 bg-white text-black font-bold text-center py-2 px-7 rounded-full">Create / Join Room</div>
             </div>
-            <div className="w-[70%] h-full">
+            <div className="w-[70%] h-full relative">
                 <div className="flex justify-between px-4 bg-blue-800 text-white rounded-tr-2xl p-3">
                     <div className="flex gap-3">
                         <div className="font-extrabold underline">ROOM CODE :</div>
@@ -23,8 +25,15 @@ export default function Dashboard(){
                     </div>
                     <div className="bg-white text-red-600 font-bold px-2 rounded-full cursor-default">Leave Room</div>
                 </div>
+                <div className="p-4 flex flex-col gap-2.5">
+                    <ChatBubble sender="Ranjan" msg="Hi everyone!" time="9:05 PM" />
+                    <ChatBubble sender="Ranjan" msg="Hi everyone!" time="10:05 PM" />
+                </div>
+                <textarea className="absolute bottom-0 resize-none rounded-br-2xl bg-white h-12 w-full focus:outline-0 p-2 text-md">
+                </textarea>
+                <div className="absolute right-4 bottom-3"><SendIcon size={20}/></div>
             </div>
         </div>
-        <div className="absolute bottom-2 left-[6%] text-red-600 cursor-default">Log out</div>
+        <div className="absolute bottom-2 left-[6%] text-red-600 hover:text-red-300 cursor-default">Log out</div>
     </div>
 }
--- components/chatbubble.tsx
+++ components/chatbubble.tsx
@@ -1,3 +1,19 @@
-export default function ChatBubble(){
-    return <div></div>
+interface bubbleProps{
+    sender: string;
+    msg: string;
+    time: string;
+}
+
+export default function ChatBubble(props: bubbleProps){
+    return <div className="">
+        <div className="pl-1">{props.sender}</div>
+        <div className="bg-blue-400 p-2 max-w-80 rounded-t-2xl rounded-br-2xl relative">
+            <div className="text-white">{props.msg}</div>
+            <div className="absolute bottom-0 right-3 text-[10px] text-white">
+                {props.time}
+            </div>
+        </div>
+        
+        
+    </div>
 }
